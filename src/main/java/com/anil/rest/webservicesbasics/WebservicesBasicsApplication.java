cc90c98319c62f41811dc676f0c24bf202c4a0ec
Anil Verma <>
2021-06-02 16:04:08 +0530

    Rest Webservices initial setup

---
+++ src/main/java/com/anil/rest/webservicesbasics/WebservicesBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.anil.rest.webservicesbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anil/rest/webservicesbasics/WebservicesBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anil.rest.webservicesbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
